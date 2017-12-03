6470bed192cc126843ef2d2e5932c476e1c56f88
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-12-03 21:57:39 +0100

    Day 3 Part 2 solved test-driven!

--- adventofcode2017/03/day3_2.py
+++ adventofcode2017/03/day3_2.py
@@ -4,24 +4,101 @@ class Grid:
         self.rings = rings
         self.n_elems = (2*rings - 1) ** 2
         self.chain = [0 for i in range(self.n_elems)]
+        self.make_coords()
+        self.fill_chain()
 
     def fill_chain(self):
         self.chain[0] = 1
-        coord = [0, 0]
         for i in range(1, self.n_elems):
-            coords = self.get_next_coord(coord)
-            self.chain[i] = self.fill_next_chain_elem(coords)
+            coord = self.get_coord_by_i(i)
+            new_val = self.fill_this_chain_elem(coord)
+            # print("I'ma fill number " + str(i) + " with " + str(new_val))
+            self.chain[i] = new_val
 
-    def get_next_coord(self, coord):
-        pass
+    def make_coords(self):
+        """
+        Coordinates are in [row, col] format and start at the center
+        of the chain at [0, 0]. For a grid with 4 rings (i.e. 49 elements),
+        the coordinates then go from [-3, -3] to [3, 3]
+        """
+        self.coords = [[0, 0]]
+        direction = "right"
+        for i in range(1, self.n_elems):
+            if direction == "right":
+                x = self.coords[i-1][0]
+                y = self.coords[i-1][1] + 1
+                if x + 1 == y:
+                    # switch walking direction from right to up in these
+                    # "lower right" corners of the grid
+                    direction = "up"
+                    
+            elif direction == "up":
+                x = self.coords[i-1][0] - 1
+                y = self.coords[i-1][1]
+                if -x == y:
+                    direction = "left"
+                    
+            elif direction == "left":
+                x = self.coords[i-1][0]
+                y = self.coords[i-1][1] - 1
+                if x == y:
+                    direction = "down"
+                    
+            elif direction == "down":
+                x = self.coords[i-1][0] + 1
+                y = self.coords[i-1][1] 
+                if -x == y:
+                    direction = "right"
+                    
+            self.coords.append([x, y])
             
-    def fill_next_chain_elem(self, coords):
-        pass
+    def get_coord_by_i(self, i):
+        return self.coords[i]
+
+    def get_neighboring_coords(self, coord):
+        return [
+            [coord[0]+dx, coord[1]+dy]
+            for dx in range(-1, 2)
+            for dy in range(-1, 2)
+            if (not (dx==0 and dy==0))  # so that the field is not its own neighbor
+            and (not abs(coord[0]+dx) >= self.rings)  # here we check that the fields are not outside of the outermost ring
+            and (not abs(coord[1]+dy) >= self.rings)
+        ]
+        
+    def fill_this_chain_elem(self, coord):
+        neighbors = self.get_neighboring_coords(coord)
+        total = 0
+        for neighbor_coord in neighbors:
+            this_val = self.get_elem_by_coord(neighbor_coord)
+            # print("I got val=" + str(this_val) + " from coord " + str(neighbor_coord))
+            total += this_val
+        return total
         
     def get_chain(self, length):
         return self.chain[0:length]
 
     def get_elem_by_i(self, i):  # actually, only used for test code
         return self.chain[i]
+    
+    def get_elem_by_coord(self, coord):
+        for i in range(self.n_elems):
+            c_i = self.get_coord_by_i(i)
+            if coord[0] == c_i[0] and coord[1] == c_i[1]:
+                return self.chain[i]
 
-grid = Grid(rings=4)
+    def get_first_greater_than(self, num):
+        for i in range(self.n_elems):
+            if self.chain[i] > num:
+                return self.chain[i]
+        return None
+
+if __name__ == "__main__":    
+    grid = Grid(rings=5)
+
+    # for i in range(20):
+    #     print("Coord " + str(i) + ": " + str(grid.get_coord_by_i(i)))
+
+    # print(grid.get_elem_by_coord([-1, -2]))
+
+    print(grid.get_chain(grid.n_elems))
+    print(grid.get_first_greater_than(368078))
---
+++ adventofcode2017/03/day3_2_tests.py
@@ -0,0 +1,40 @@
+# Just run this file with 'python 2_tests.py'
+import unittest
+from day3_2 import Grid
+
+class GridTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.grid = Grid(rings=4)
+
+    def test_get_coord_by_id(self):
+        self.assertEqual(self.grid.get_coord_by_i(0), [0, 0])
+        self.assertEqual(self.grid.get_coord_by_i(17), [-1, -2])        
+    
+    def test_first_element(self):
+        """
+        This tests that the first element in the Ulam spiral is 1
+        """
+        self.assertEqual(self.grid.get_elem_by_i(0), 1)
+
+    def test_get_elem_by_i(self):
+        self.assertEqual(self.grid.get_elem_by_i(1), 1)
+        self.assertEqual(self.grid.get_elem_by_i(2), 2)
+        self.assertEqual(self.grid.get_elem_by_i(3), 4)
+        self.assertEqual(self.grid.get_elem_by_i(4), 5)
+        self.assertEqual(self.grid.get_elem_by_i(5), 10)
+        self.assertEqual(self.grid.get_elem_by_i(6), 11)
+        self.assertEqual(self.grid.get_elem_by_i(7), 23)
+
+    def test_get_chain(self):
+        self.assertEqual(self.grid.get_chain(1), [1])
+        self.assertEqual(self.grid.get_chain(5), [1, 1, 2, 4, 5])        
+
+    def test_get_first_greater_than(self):
+        self.assertEqual(self.grid.get_first_greater_than(8), 10)
+        self.assertEqual(self.grid.get_first_greater_than(122), 133)
+        self.assertEqual(self.grid.get_first_greater_than(1234), 1968)
+        self.assertEqual(self.grid.get_first_greater_than(99999999), None)
+        
+if __name__ == "__main__":
+    unittest.main()
