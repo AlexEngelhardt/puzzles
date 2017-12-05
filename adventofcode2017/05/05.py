52d246f9325560ede8f27792d23d603d7a4499fd
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-12-05 21:14:27 +0100

    AoC day 5

---
+++ adventofcode2017/05/05.py
@@ -0,0 +1,20 @@
+numbers = []
+with open("input.txt", "r") as f:
+    for num in f:
+        numbers.append(int(num))
+
+print(numbers)
+
+n_steps = 0
+i = 0
+while i < len(numbers):
+    forward_steps = numbers[i]
+    if numbers[i] >= 3:  # comment out for part 1
+        numbers[i] -= 1  # comment out for part 1
+    else:                # comment out for part 1
+        numbers[i] += 1
+
+    n_steps += 1
+    i = i + forward_steps
+
+print(n_steps)
