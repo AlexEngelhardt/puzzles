572de7f5c1daf632d70c8621fbf938a58c6fe653
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2017-12-05 21:31:06 +0100

    one-liner to remove the last empty line

--- adventofcode2017/05/05.py
+++ adventofcode2017/05/05.py
@@ -1,7 +1,8 @@
 numbers = []
 with open("input.txt", "r") as f:
-    for num in f:
-        numbers.append(int(num))
+    # for num in f:
+    #     numbers.append(int(num))
+    numbers = list(map(int, [boop for boop in f.read().split("\n") if boop != ""]))
 
 print(numbers)
 
@@ -9,10 +10,10 @@ n_steps = 0
 i = 0
 while i < len(numbers):
     forward_steps = numbers[i]
-    if numbers[i] >= 3:  # comment out for part 1
-        numbers[i] -= 1  # comment out for part 1
-    else:                # comment out for part 1
-        numbers[i] += 1
+    # if numbers[i] >= 3:  # comment out for part 1
+    #     numbers[i] -= 1  # comment out for part 1
+    # else:                # comment out for part 1
+    numbers[i] += 1
 
     n_steps += 1
     i = i + forward_steps
