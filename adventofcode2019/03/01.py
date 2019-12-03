5f2013c0d78dfcf0c893ab42e4e5cf8ce27a064e
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2019-12-03 10:13:23 +0100

    day 3 kudasai

---
+++ adventofcode2019/03/01.py
@@ -0,0 +1,74 @@
+# Don't model the playing field as a 2d-array,
+# the memory won't suffice.
+
+# Instead, create a list of 2-tuples that reflect the sequential
+# coordinates.
+
+# Then, find identical coordinates.
+
+# No! Use a dict with a 2-tuple key. Where the value is 2, the paths collide!
+
+debug = False
+
+if debug:
+    filename = 'test-input'
+else:
+    filename = 'input'
+
+with open(filename) as f:
+    path1 = f.readline().strip().split(',')
+    path2 = f.readline().strip().split(',')
+
+if debug:
+    print(path1)
+    print(path2)
+
+
+def create_coord_set(path):
+    # The current position:
+    path_pos = [0, 0]
+
+    # The set containing the travelled path:
+    path_set = set()
+
+    # No, don't add the first coord, because that would count as an
+    # intersection with minimal distance :)
+    # path_set.add((0, 0))
+
+    for steps in path:
+        if steps[0] == 'R':
+            axis, delta = 0, 1
+        elif steps[0] == 'L':
+            axis, delta = 0, -1
+        elif steps[0] == 'U':
+            axis, delta = 1, 1
+        elif steps[0] == 'D':
+            axis, delta = 1, -1
+
+        nsteps = int(steps[1:])
+
+        for i in range(nsteps):
+            path_pos[axis] += delta
+            path_set.add(tuple(path_pos))
+
+    return(path_set)
+
+
+path_set_1 = create_coord_set(path1)
+path_set_2 = create_coord_set(path2)
+
+if debug:
+    print(path_set_1)
+
+intersections = list()
+distances = list()
+
+for xy in path_set_1:
+    if xy in path_set_2:
+        intersections.append(xy)
+        distances.append(abs(xy[0]) + abs(xy[1]))
+
+if debug:
+    print(intersections)
+
+print(min(distances))
