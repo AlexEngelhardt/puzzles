339451b3f35ad3fcfbb9a6d731d1df8019bb678c
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2019-12-04 17:17:49 +0100

    aoc 2015 d4

---
+++ adventofcode2015/04/02.py
@@ -0,0 +1,18 @@
+import hashlib
+
+hashlib.md5('abc'.encode('utf-8')).hexdigest()
+
+# salt = 'abcdef'
+salt = 'yzbqklnj'
+add = 1
+
+check = salt + str(add)
+
+while hashlib.md5((salt + str(add)).encode('utf-8')).hexdigest()[:6] != '000000':
+    add += 1
+    if add > 10_000_000:
+        print('Timeout :D')
+        break
+
+print(add)
+
