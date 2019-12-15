9cc7fbdf03392cfb336402f2328d52e400af3a79
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2019-12-15 10:05:53 +0100

    day 9 part 1 gozaimasu

--- adventofcode2019/09/01.py
+++ adventofcode2019/09/01.py
@@ -3,5 +3,11 @@ from computer import Computer
 with open('input') as f:
     memory = list(map(int, f.read().split(',')))
 
-computer = Computer(memory, debug=True)
+computer = Computer(memory, inputs=[1], debug=False)
 computer.run()
+
+# 203 is too low
+# BUT:
+# It will perform a series of checks on each opcode, output
+# any opcodes (and the associated parameter modes) that seem to be functioning
+# incorrectly,
--- adventofcode2019/09/computer.py
+++ adventofcode2019/09/computer.py
@@ -3,11 +3,12 @@ class ExitOpcode(Exception):
 
 
 class Computer():
-    def __init__(self, memory, inputs=[], debug=False):
+    def __init__(self, memory, inputs=[], relative_base=0, debug=False):
         self.memory = memory
         self.debug = debug
         self.inputs = inputs  # a *stack* that gets popped from left-to-right
         self.iptr = 0  # instruction pointer
+        self.relative_base = relative_base
 
     def step(self):
         instruction = self.memory[self.iptr]
@@ -33,6 +34,8 @@ class Computer():
             self.lessthan(modes)
         elif opcode == 8:
             self.equals(modes)
+        elif opcode == 9:
+            self.shift_relative_base(modes)
         elif opcode == 99:
             raise ExitOpcode('buh-bye!')
         else:
@@ -48,18 +51,23 @@ class Computer():
                 if self.debug:
                     print('exit opcode reached')
                 raise
-            if opcode == 4:
-                # In case we reached a output, *pause* the computer.
-                # This started to be important in day 7 part 2
-                break
+            # if opcode == 4:
+            #     # In case we reached a output, *pause* the computer.
+            #     # This started to be important in day 7 part 2
+            #     break
         return self.last_output
 
-    def get(self, pos, mode):
+    def get_pos(self, pos, mode):
         if mode == 0:
             position = self.memory[pos]
         elif mode == 1:
             position = pos
+        elif mode == 2:
+            position = self.memory[pos] + self.relative_base
+        return position
 
+    def get_val(self, pos, mode):
+        position = self.get_pos(pos, mode)
         if position > len(self.memory):
             return 0
         else:
@@ -68,33 +76,34 @@ class Computer():
     def set(self, pos, value):
         # the position is already the "final", absolute memory position
 
-        if pos > len(self.memory):
-            raise NotImplementedError('Write out of bounds')
+        if (pos + 1) > len(self.memory):
+            # Writing out of bounds: Enlarge the memory
+            self.memory.extend([0] * (pos - len(self.memory) + 1))
 
         self.memory[pos] = value
 
     def add(self, modes):
-        x1 = self.get(pos=self.iptr + 1, mode=modes % 10)
-        x2 = self.get(pos=self.iptr + 2, mode=modes // 10 % 10)
+        x1 = self.get_val(pos=self.iptr + 1, mode=modes % 10)
+        x2 = self.get_val(pos=self.iptr + 2, mode=modes // 10 % 10)
         # Parameters that an instruction writes to will never be in immediate
         # mode, but only in position mode:
-        target = self.memory[self.iptr + 3]
+        target = self.get_pos(self.iptr + 3, mode=modes // 100 % 10)
         if self.debug:
             print(f'adding {x1} + {x2} into position {target}')
         self.set(target, x1 + x2)
         self.iptr += 4
 
     def multiply(self, modes):
-        x1 = self.get(pos=self.iptr + 1, mode=modes % 10)
-        x2 = self.get(pos=self.iptr + 2, mode=modes // 10 % 10)
-        target = self.memory[self.iptr + 3]
+        x1 = self.get_val(pos=self.iptr + 1, mode=modes % 10)
+        x2 = self.get_val(pos=self.iptr + 2, mode=modes // 10 % 10)
+        target = self.get_pos(self.iptr + 3, mode=modes // 100 % 10)
         if self.debug:
             print(f'multiplying {x1} * {x2} into position {target}')
         self.set(target, x1 * x2)
         self.iptr += 4
 
     def set_input(self, modes):
-        target = self.memory[self.iptr + 1]
+        target = self.get_pos(self.iptr + 1, mode=modes % 10)
         if self.debug:
             print(f'Popping input value {self.inputs[0]}')
         if len(self.inputs) < 1:
@@ -104,9 +113,8 @@ class Computer():
         self.iptr += 2
 
     def get_output(self, modes):
-        output = self.get(pos=self.iptr + 1, mode=modes % 10)
-        if self.debug:
-            print(f'Output: {output}')
+        output = self.get_val(pos=self.iptr + 1, mode=modes % 10)
+        print(f'Output: {output}')
         self.iptr += 2
         self.last_output = output
         return output
@@ -116,9 +124,9 @@ class Computer():
         pointer to the value from the second parameter. Otherwise, it does
         nothing.
         """
-        check = self.get(pos=self.iptr + 1, mode=modes % 10)
+        check = self.get_val(pos=self.iptr + 1, mode=modes % 10)
         if check:
-            self.iptr = self.get(pos=self.iptr + 2, mode=modes // 10 % 10)
+            self.iptr = self.get_val(pos=self.iptr + 2, mode=modes // 10 % 10)
         else:  # Only update the iptr if the jump did not happen!
             self.iptr += 3
 
@@ -127,9 +135,9 @@ class Computer():
         pointer to the value from the second parameter. Otherwise, it does
         nothing.
         """
-        check = self.get(pos=self.iptr + 1, mode=modes % 10)
+        check = self.get_val(pos=self.iptr + 1, mode=modes % 10)
         if check == 0:
-            self.iptr = self.get(pos=self.iptr + 2, mode=modes // 10 % 10)
+            self.iptr = self.get_val(pos=self.iptr + 2, mode=modes // 10 % 10)
         else:  # Only update the iptr if the jump did not happen!
             self.iptr += 3
 
@@ -138,9 +146,9 @@ class Computer():
         stores 1 in the position given by the third parameter. Otherwise,
         it stores 0.
         """
-        p1 = self.get(pos=self.iptr + 1, mode=modes % 10)
-        p2 = self.get(pos=self.iptr + 2, mode=modes // 10 % 10)
-        target = self.memory[self.iptr + 3]
+        p1 = self.get_val(pos=self.iptr + 1, mode=modes % 10)
+        p2 = self.get_val(pos=self.iptr + 2, mode=modes // 10 % 10)
+        target = self.get_pos(self.iptr + 3, mode=modes // 100 % 10)
         value = 1 if p1 < p2 else 0
         self.set(target, value)
         self.iptr += 4
@@ -150,9 +158,19 @@ class Computer():
         stores 1 in the position given by the third parameter. Otherwise,
         it stores 0.
         """
-        p1 = self.get(pos=self.iptr + 1, mode=modes % 10)
-        p2 = self.get(pos=self.iptr + 2, mode=modes // 10 % 10)
-        target = self.memory[self.iptr + 3]
+        p1 = self.get_val(pos=self.iptr + 1, mode=modes % 10)
+        p2 = self.get_val(pos=self.iptr + 2, mode=modes // 10 % 10)
+        target = self.get_pos(self.iptr + 3, mode=modes // 100 % 10)
         value = 1 if p1 == p2 else 0
         self.set(target, value)
         self.iptr += 4
+
+    def shift_relative_base(self, modes):
+        """Opcode 9 adjusts the relative base by the value of its only
+        parameter. The relative base increases (or decreases, if the value is
+        negative) by the value of the parameter.
+        """
+        # 'modes' is needed here too!
+        shift = self.get_val(self.iptr + 1, mode=modes % 10)
+        self.relative_base += shift
+        self.iptr += 2
