b0d369a91e3ddf6a28d4c539dc0be1fd601ea949
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2020-12-15 09:49:11 +0100

    2020 day 15 part 1, but it's *not* pretty

---
+++ adventofcode2020/15/01.py
@@ -0,0 +1,44 @@
+debug = False
+
+if debug:
+    filename = 'test_input'
+    n_turns = 2020 # 10
+else:
+    filename = 'input'
+    n_turns = 2020
+
+with open(filename) as f:
+    numbers = list(map(int, f.readline().split(",")))
+
+len_input_numbers = len(numbers)
+print(numbers)
+prev_numbers = dict()  # key: the number; value: a list of turn IDs where this number was seen
+
+# Fill prev_numbers:
+for i, number in enumerate(numbers[:-1]):
+    turn = i+1
+    if number not in prev_numbers:
+        prev_numbers[number] = list()
+    prev_numbers[number].append(turn)
+
+print(prev_numbers)
+
+# Now generate new numbers until turn 2020:
+for i in range(n_turns - len(numbers)):
+    turn = i + len_input_numbers + 1
+
+    last_number = numbers[-1]
+    if last_number not in prev_numbers:
+        prev_numbers[last_number] = [turn-1]
+        new_number = 0
+    else:
+        prev_numbers[last_number].append(turn - 1)
+        new_number = prev_numbers[last_number][-1] - prev_numbers[last_number][-2]
+
+    numbers.append(new_number)
+
+
+
+print(prev_numbers)
+print(numbers)
+print(numbers[-1])
